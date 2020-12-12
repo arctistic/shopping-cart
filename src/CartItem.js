7f1bffcf305bb79891f6a2168845e9da95c316c3
Akhil Ratna Chettri <chettri.akhilratna@gmail.com>
2020-12-13 03:21:21 +0530

    setState in depth

--- src/CartItem.js
+++ src/CartItem.js
@@ -9,7 +9,78 @@ class CartItem extends React.Component {
             qty: 1,
             img: ''
         }
+        // this.increaseQuantity = this.increaseQuantity.bind(this)
+        this.testing();
     }
+
+    testing(){
+        const promise = new Promise((resolve, reject) => {
+            setTimeout(() => {
+                resolve('done');
+            }, 5000);
+        })
+        promise.then(() => {
+            this.setState({ qty: this.state.qty + 10 });
+            this.setState({ qty: this.state.qty + 10 });
+            this.setState({ qty: this.state.qty + 10 });
+            // setState works like a synchronous call in a promise
+            // thus all setState will add up and the code below will only execute after the setState
+            console.log('state', this.state)
+        })
+    }
+
+    increaseQuantity = () => {
+        /* METHOD 1 : setState - passing an object
+        The setState func comes from React.Component as we are inheriting from it we are able to use it (this is one way of doing it, if the previous state is not required*/
+        /**BATCHING: if there are many setState functions, the component is rendered only ONCE, taking the modified attribute of the object that it returns on the last setState function (shallow merging) */
+        // this.setState({
+        //     qty: this.state.qty + 1
+        // })
+        // this.setState({
+        //     qty: this.state.qty + 2,
+        //     title: 'Hey'
+        // }, () => {})
+
+        
+        /*METHOD 2 : setState - passing a funtion
+        If the previous state is required - passing a callback function to setState function. the callback function is an arrow func. the argument to the arrow function is the previous state, i.e., previous this*/
+        /**Batching DOES NOT HAPPEN, a queue is mantained and all the setState function is computed */
+        // this.setState((prevState)=>{
+        //     return {
+        //         qty: prevState.qty + 1
+        //     }
+        // })
+        // this.setState((prevState)=>{
+        //     return {
+        //         title: 'kabd',
+        //         qty: prevState.qty + 0
+        //     }
+        // })
+
+
+        /*setState function is ASYNCHRONOUS therefore the code written after the setState function does not wait for the setState function to end it will run anyway.
+        To wait for the setState function to end we can write the following code inside the callback function as another parameter of setState function */
+        this.setState((prevState)=>{
+            return {
+                qty: prevState.qty + 1
+            }
+        }, () => {
+            console.log('this.state', this.state);
+        })
+        
+    }
+
+    decreaseQuantity = () => {
+        const { qty } = this.state
+        this.setState((prevState => {
+            if (qty > 1){
+                return {qty: prevState.qty - 1};
+            }else{
+                return;
+            }
+        }))
+    }
+
     render () {
         const {price, title, qty} = this.state;
         return (
@@ -23,9 +94,25 @@ class CartItem extends React.Component {
                     <div style={{color: '#777'}}>Qty: {qty}</div>
                     <div className="cart-item-actions">
                         {/* Buttons */}
-                        <img alt="increase" className="action-icons" src="https://www.flaticon.com/svg/static/icons/svg/992/992651.svg" />
-                        <img alt="decrease" className="action-icons" src="https://www.flaticon.com/svg/static/icons/svg/1828/1828906.svg" />
-                        <img alt="delete" className="action-icons" src="https://www.flaticon.com/svg/static/icons/svg/1214/1214428.svg" />
+                        <img 
+                            alt="decrease" 
+                            className="action-icons" 
+                            src="https://www.flaticon.com/svg/static/icons/svg/1828/1828906.svg" 
+                            onClick={this.decreaseQuantity}
+                        />
+                        <img 
+                            alt="increase" 
+                            className="action-icons" 
+                            src="https://www.flaticon.com/svg/static/icons/svg/992/992651.svg" 
+                            onClick={this.increaseQuantity 
+                                /* Need to bind this either here or constructor
+                                or make use of Arrow functions*/}
+                        />
+                        <img 
+                            alt="delete" 
+                            className="action-icons" 
+                            src="https://www.flaticon.com/svg/static/icons/svg/1214/1214428.svg" 
+                        />
                     </div>
                 </div>
             </div>
